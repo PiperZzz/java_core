f1363b7f5e89bdb78897ad69cdc551958f426cad
Panda77 <PiperZzz@users.noreply.github.com>
2023-05-21 21:56:12 -0400

    string 1

--- src/main/java/base/core/basic/MyString.java
+++ src/main/java/base/core/basic/MyString.java
@@ -5,8 +5,13 @@ public class MyString {
 
     StringBuffer sb = new StringBuffer("Hello String Buffer"); // thread safe, synchronized, low performance
     StringBuilder sb2 = new StringBuilder("Hello String Builder");  // not thread safe, not synchronized, high performance
+    // SQL query is good practice for StringBuffer and StringBuilder
 
     // String pool is a special storage area in Java heap that stores String literals
 
-    // String equals() and == operator difference
+    // New String () vs String Literal 
+
+    // String equals() vs == operator difference
+
+    // The common methods of the String class include length(), charAt(), substring(), equals(), indexOf(), toLowerCase(), toUpperCase(), trim(), etc.
 }
