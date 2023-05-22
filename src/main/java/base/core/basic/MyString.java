b32509ec2ff4b67e1db24f0aea4051069cf5775c
Panda77 <PiperZzz@users.noreply.github.com>
2023-05-21 21:02:41 -0400

    string 1

--- src/main/java/base/core/basic/MyString.java
+++ src/main/java/base/core/basic/MyString.java
@@ -1,10 +1,12 @@
 package base.core.basic;
 
 public class MyString {
-    String s = "Hello String"; // String is a immutable class
+    String s = "Hello String"; //immutable, thread safe, high performance
 
-    StringBuffer sb = new StringBuffer("Hello String Buffer"); // thread safe
-    StringBuilder sb2 = new StringBuilder("Hello String Builder");  // not thread safe, better performance
+    StringBuffer sb = new StringBuffer("Hello String Buffer"); // thread safe, synchronized, low performance
+    StringBuilder sb2 = new StringBuilder("Hello String Builder");  // not thread safe, not synchronized, high performance
 
     // String pool is a special storage area in Java heap that stores String literals
+
+    // String equals() and == operator difference
 }
