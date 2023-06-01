00ef6df0d52410cfe63cba95a7cc3b687d6b4732
Panda77 <PiperZzz@users.noreply.github.com>
2023-06-01 19:54:36 -0400

    String comparing

--- src/main/java/base/core/CoreApplication.java
+++ src/main/java/base/core/CoreApplication.java
@@ -3,11 +3,14 @@ package base.core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import base.core.basic.MyString;
+
 @SpringBootApplication
 public class CoreApplication {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		MyString.stringCompare();
 		SpringApplication.run(CoreApplication.class, args);
 	}
 
--- src/main/java/base/core/basic/MyString.java
+++ src/main/java/base/core/basic/MyString.java
@@ -7,11 +7,26 @@ public class MyString {
     StringBuilder sb2 = new StringBuilder("Hello String Builder");  // not thread safe, not synchronized, high performance
     // SQL query is good practice for StringBuffer and StringBuilder
 
+
+    public static void stringCompare() {
     // String pool is a special storage area in Java heap that stores String literals
 
     // New String () vs String Literal 
 
     // String equals() vs == operator difference
+
+    String s1 = "abc";
+    String s2 = "abc";
+
+    String s3 = new String("abc");
+    String s4 = new String("abc");
+    
+    System.out.println(s1 == s2); // true // String pool String literal  
+    System.out.println(s3 == s4); // false // Heap memory New String() object reference
+
+    System.out.println(s1.equals(s2)); // true  // String literal   
+    System.out.println(s3.equals(s4)); // true  // New String()
+    }
 
     // The common methods of the String class include length(), charAt(), substring(), equals(), indexOf(), toLowerCase(), toUpperCase(), trim(), etc.
 }
