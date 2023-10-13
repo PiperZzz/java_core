9676aa5faaf76b6d4bf1a60152fbd8ee6b69efc7
Panda77 <PiperZzz@users.noreply.github.com>
2023-10-13 16:25:49 -0400

    typo

--- src/main/java/base/core/basic/MyMap.java
+++ src/main/java/base/core/basic/MyMap.java
@@ -6,19 +6,19 @@ import java.util.Map;
 public class MyMap {
     Map<String, String> hashMap = new HashMap<>();
 
-    public void entrySetTrerse() {
+    public void entrySetTraverse() {
         for (Map.Entry<String, String> entry : hashMap.entrySet()) {
             System.out.println(entry.getKey() + " " + entry.getValue());
         }
     }
     
-    public void keySetTrerse() {
+    public void keySetTraverse() {
         for (String key : hashMap.keySet()) {
             System.out.println(key + " " + hashMap.get(key));
         }
     }
 
-    public void forEachTrerse() {
+    public void forEachTraverse() {
         hashMap.forEach((k, v) -> System.out.println((k + " " + v)));
     }
 }
