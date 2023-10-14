5f2533297603a0a7d57eec5712612eec60b95fcd
Panda77 <fight4uking@gmail.com>
2023-10-14 18:20:54 -0400

    for-each

--- src/main/java/base/core/leetcode/MaxProfit.java
+++ src/main/java/base/core/leetcode/MaxProfit.java
@@ -5,14 +5,11 @@ public class MaxProfit {
             int minPrice = Integer.MAX_VALUE; // 初始化最低股价为正无穷大
             int maxProfit = 0; // 初始化最大利润为0
             
-            for (int i = 0; i < prices.length; i++) {
-                if (prices[i] < minPrice) {
-                    minPrice = prices[i]; // 更新最低股价
-                } else if (prices[i] - minPrice > maxProfit) {
-                    maxProfit = prices[i] - minPrice; // 更新最大利润
-                }
+            for (int price : prices) {
+                minPrice = Math.min(minPrice, price); // 更新最低股价
+                maxProfit = Math.max(maxProfit, price - minPrice); // 更新最大利润
             }
-            
+                
             return maxProfit;
         }
 }
