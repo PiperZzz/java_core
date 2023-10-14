021329a3fad1ad6b372e7952bed89fce06f2c0e0
Panda77 <PiperZzz@users.noreply.github.com>
2023-10-14 12:14:49 -0400

    two sum

--- src/main/java/base/core/CoreApplication.java
+++ src/main/java/base/core/CoreApplication.java
@@ -3,14 +3,18 @@ package base.core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import base.core.basic.MyString;
+import java.util.*;
 
 @SpringBootApplication
 public class CoreApplication {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-		MyString.stringCompare();
+
+		int [] nums = {2, 7, 11, 15};
+
+		System.out.println(Arrays.toString(nums));
+
 		SpringApplication.run(CoreApplication.class, args);
 	}
 
