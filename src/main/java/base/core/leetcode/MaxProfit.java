5059b832013f0230700c72179f8ad9419a641f01
Panda77 <fight4uking@gmail.com>
2023-10-15 15:05:41 -0400

    formating

--- src/main/java/base/core/leetcode/MaxProfit.java
+++ src/main/java/base/core/leetcode/MaxProfit.java
@@ -2,14 +2,14 @@ package base.core.leetcode;
 
 public class MaxProfit {
     public static int maxProfit(int[] prices) {
-            int minPrice = Integer.MAX_VALUE; // 初始化最低股价为正无穷大
-            int maxProfit = 0; // 初始化最大利润为0
-            
-            for (int price : prices) {
-                minPrice = Math.min(minPrice, price); // 更新最低股价
-                maxProfit = Math.max(maxProfit, price - minPrice); // 更新最大利润
-            }
-                
-            return maxProfit;
+        int minPrice = Integer.MAX_VALUE; // 初始化最低股价为正无穷大
+        int maxProfit = 0; // 初始化最大利润为0
+        
+        for (int price : prices) {
+            minPrice = Math.min(minPrice, price); // 更新最低股价
+            maxProfit = Math.max(maxProfit, price - minPrice); // 更新最大利润
         }
+            
+        return maxProfit;
+    }
 }
