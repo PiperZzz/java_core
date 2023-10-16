dfed24db7967c587ef0cd631d69bbf02b1d76fd7
Panda77 <PiperZzz@users.noreply.github.com>
2023-10-16 12:12:34 -0400

    top k freq elements

--- src/main/java/base/core/CoreApplication.java
+++ src/main/java/base/core/CoreApplication.java
@@ -1,7 +1,12 @@
 package base.core;
 
+import base.core.leetcode.TwoSum;
+import java.util.Arrays;
+
 public class CoreApplication {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		int[] array = TwoSum.twoSum(new int[]{2, 11, 15, 7}, 9);
+		System.out.println(Arrays.toString(array));
 	}
 }
--- src/main/java/base/core/leetcode/sort/TopKFrequentElements.java
+++ src/main/java/base/core/leetcode/sort/TopKFrequentElements.java
@@ -1,5 +1,36 @@
 package base.core.leetcode.sort;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class TopKFrequentElements {
-    
+    public List<Integer> topKFrequent(int[] nums, int k) {
+        // 使用哈希表记录每个元素的频率
+        Map<Integer, Integer> frequencyMap = new HashMap<>();
+        for (int num : nums) {
+            frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1); //将num的频率加1（num是key，频率是value）
+        }
+
+        // 使用TreeMap，按照频率作为键，元素列表作为值
+        TreeMap<Integer, List<Integer>> frequencyTreeMap = new TreeMap<>();
+        for (int num : frequencyMap.keySet()) {
+            int frequency = frequencyMap.get(num);
+            if (!frequencyTreeMap.containsKey(frequency)) {
+                frequencyTreeMap.put(frequency, new ArrayList<>());
+            }
+            frequencyTreeMap.get(frequency).add(num);
+        }
+
+        // 从高频率到低频率遍历TreeMap，获取Top K频率元素
+        List<Integer> result = new ArrayList<>();
+        while (result.size() < k) {
+            Map.Entry<Integer, List<Integer>> entry = frequencyTreeMap.pollLastEntry();
+            result.addAll(entry.getValue());
+        }
+
+        return result;
+    }
 }
