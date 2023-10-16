b415830c1da185f58812d43cc61dc56708839cc2
Panda77 <PiperZzz@users.noreply.github.com>
2023-10-16 12:14:31 -0400

    clean up

--- src/main/java/base/core/CoreApplication.java
+++ src/main/java/base/core/CoreApplication.java
@@ -1,12 +1,12 @@
 package base.core;
 
-import base.core.leetcode.TwoSum;
 import java.util.Arrays;
 
 public class CoreApplication {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-		int[] array = TwoSum.twoSum(new int[]{2, 11, 15, 7}, 9);
+		int[] array = {1,2,3,4,5};
 		System.out.println(Arrays.toString(array));
+		System.out.println("Hello World!");
 	}
 }
