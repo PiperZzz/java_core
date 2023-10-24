8a81f3ee38bd12e11588d11b6d8df0bb94eca1e6
Panda77 <PiperZzz@users.noreply.github.com>
2023-10-24 18:38:53 -0400

    palindrome

---
+++ src/main/java/base/core/leetcode/two_pointers/Palindrome.java
@@ -0,0 +1,22 @@
+package base.core.leetcode.two_pointers;
+
+public class Palindrome {
+    public boolean isPalindrome(String str) {
+        // 删除非字母数字字符并将字符串转换为小写
+        str = str.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        
+        // 使用双指针检查回文性质
+        int left = 0;
+        int right = str.length() - 1;
+        
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        
+        return true;
+    }    
+}
