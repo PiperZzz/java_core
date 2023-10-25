81a620368437be4eae8075b18d55e07ed9b65bed
Panda77 <PiperZzz@users.noreply.github.com>
2023-10-25 10:59:58 -0400

    clean up

--- src/main/java/base/core/basic/collection/MyMap.java
+++ src/main/java/base/core/basic/collection/MyMap.java
@@ -10,60 +10,64 @@ public class MyMap {
 
         /* Map不是Collection内的接口 */
         
-        Map<Character, Integer> map = new HashMap<>();
+        Map<Character, Integer> hashMap = new HashMap<>();
         
-        map.put('a', 1);
-        map.put('b', 1);
-        map.put('c', 1);
+        hashMap.put('a', 1);
+        hashMap.put('b', 1);
+        hashMap.put('c', 1);
 
-        Set<Character> keySet = map.keySet();
+        Set<Character> keySet = hashMap.keySet();
         
-        Collection<Integer> values = map.values();
+        Collection<Integer> values = hashMap.values();
 
         // get()方法的作用是：如果Map中包含这个key，则返回key对应的value，否则返回null
-        map.get('d');
+        hashMap.get('d');
 
         // getOrDefault()方法的作用是：如果Map中包含这个key，则返回key对应的value，否则返回指定的默认值,这里是0
-        map.getOrDefault('d', 1);
+        hashMap.getOrDefault('d', 1);
 
         // containsKey()方法的作用是：如果Map中包含这个key，则返回true，否则返回false
-        map.containsKey('d');
+        hashMap.containsKey('d');
 
         // containsValue()方法的作用是：如果Map中包含这个value，则返回true，否则返回false
-        map.containsValue(1);
+        hashMap.containsValue(1);
 
         // size()方法的作用是：返回Map中键值对的数量
-        map.size();
+        hashMap.size();
 
         // putIfAbsent()方法的作用是：如果Map中不包含这个key，则添加这个key和value，否则不做任何操作
-        map.putIfAbsent('d', 1);
+        hashMap.putIfAbsent('d', 1);
 
-        map.computeIfAbsent(null, null);
+        hashMap.computeIfAbsent(null, null);
 
-        map.computeIfPresent(null, null);
+        hashMap.computeIfPresent(null, null);
 
-        Set<Map.Entry<Character, Integer>> entrySet = map.entrySet();
+        Set<Map.Entry<Character, Integer>> entrySet = hashMap.entrySet();
 
         for (Map.Entry<Character, Integer> entry : entrySet) {
             System.out.println(entry.getKey() + " " + entry.getValue());
         }
     }
 
-    Map<String, String> hashMap = new HashMap<>();
-
     public void entrySetTraverse() {
+        Map<String, String> hashMap = new HashMap<>();
+
         for (Map.Entry<String, String> entry : hashMap.entrySet()) {
             System.out.println(entry.getKey() + " " + entry.getValue());
         }
     }
     
     public void keySetTraverse() {
+        Map<String, String> hashMap = new HashMap<>();
+
         for (String key : hashMap.keySet()) {
             System.out.println(key + " " + hashMap.get(key));
         }
     }
 
     public void forEachTraverse() {
+        Map<String, String> hashMap = new HashMap<>();
+
         hashMap.forEach((k, v) -> System.out.println((k + " " + v)));
     }
 }
