b88462d82e8f8ca52a4220dd1dc075e6a2fa02fe
Panda77 <PiperZzz@users.noreply.github.com>
2023-11-08 17:32:11 -0500

    map

--- src/main/java/base/core/basic/collection/MyMap.java
+++ src/main/java/base/core/basic/collection/MyMap.java
@@ -25,43 +25,51 @@ public class MyMap {
         /*  */
         Collection<Integer> values = hashMap.values();
 
+        
+        hashMap.getOrDefault("d", 1);
         /* 如果Map中包含这个key，则返回key对应的value，否则返回指定的默认值（这里是1）
          * 注意：这个默认值并不会对Map中的键值对产生任何影响
          * 如果默认值是null，则这个方法的作用和get()完全方法一样
          */
-        hashMap.getOrDefault("d", 1);
 
-
-        /* 如果Map中包含这个key，则返回key对应的value，否则返回null */
+        
         hashMap.get("d");
+        /* 如果Map中包含这个key，则返回key对应的value，否则返回null */
 
 
+        hashMap.putIfAbsent("d", 1);
         /* 如果Map中不包含这个key，则添加这对key和value，并返回null
          * 如果Map中包含这个key，则不对Map做任何操作，并直接返回key对应的value
          * 它和put()方法的区别是：put()方法会覆盖原有的value，而putIfAbsent()不会
          * 它们在返回值的逻辑上是一致的，都是key存在返回旧值，key不存在返回null
          */
-        hashMap.putIfAbsent("d", 1);
 
 
-        /* 该方法和putIfAbsent()相似的地方是：都在key不存在时添加key和value
+        hashMap.computeIfAbsent("d", key-> key.length());
+        /* computeIfAbsent()方法和putIfAbsent()相似的地方是：都在key不存在时添加key和value
          * 但不同是：1、第二个参数允许函数式接口，执行一个和Key相关的操作，返回一个Value
          * 2、key不存在则添加key和计算过的value，这里和putIfAbsent不同，它会返回新值（计算后的value），而不是旧值null
-         * 如果key存在则不会有任何操作，也不返回任何值，注意这点也和putIfAbsent()不同，putIfAbsent()在key存在时会返回旧值
+         * 如果key存在则不会有任何操作，返回现有值（旧值），这点和putIfAbsent()是一致的
          */
-        hashMap.computeIfAbsent("d", key-> key.length());
 
-        /*  */
-        hashMap.computeIfPresent(null, null);
 
-        // containsKey()方法的作用是：如果Map中包含这个key，则返回true，否则返回false
+        hashMap.computeIfPresent("d", (key, value) -> value + key.length());
+        /* 如果Map中包含key，则可以利用其第二个参数（函数式接口）执行一个和Key和Value相关的操作，返回一个Value
+         * 这点和computeIfAbsent()是不同的，computeIfAbsent()的第二个参数是只和Key相关的操作，返回一个Value
+         * 这个区别和它们的名字也是一致的，computeIfPresent()是在key存在时才会执行操作，自然也期待value存在
+         * 而computeIfAbsent()是在key不存在时才会执行操作，自然也期待value不存在
+         * 如果key不存在，computeIfPresent()则不会有任何操作，返回null
+         */
+
+        
         hashMap.containsKey("d");
+        /*  如果Map中包含这个key，则返回true，否则返回false */
 
-        // containsValue()方法的作用是：如果Map中包含这个value，则返回true，否则返回false
         hashMap.containsValue(1);
+        /* 如果Map中包含这个value，则返回true，否则返回false */
 
-        // 返回Map中键值对（Entry）的数量
-        hashMap.size();        
+        hashMap.size();
+        /* 返回Map中键值对（Entry）的数量 */        
     }
 
     public void mapTraverse() {
