64eb8537c7c51e921c4450d43b1286376ef68179
Panda77 <PiperZzz@users.noreply.github.com>
2024-01-02 18:59:02 -0500

    batch persist data

---
+++ src/main/java/base/core/spring_data/service/UserService.java
@@ -0,0 +1,32 @@
+package base.core.spring_data.service;
+
+import java.util.List;
+
+import base.core.spring_data.model.Users;
+import base.core.spring_data.dao.UsersRepository;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private final UsersRepository usersRepository;
+
+    public UserService(UsersRepository usersRepository) {
+        this.usersRepository = usersRepository;
+    }
+
+    @Transactional
+    public void saveUsers(List<Users> usersList) {
+        for (Users users : usersList) {
+            usersRepository.save(users);
+        }
+    }
+
+    public void saveAllUsers(List<Users> usersList) {
+        usersRepository.saveAll(usersList);
+    }
+}
