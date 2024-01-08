8cebbe50daccde67e5aff4e15d4541b13d4b0065
Panda77 <PiperZzz@users.noreply.github.com>
2024-01-08 17:14:44 -0500

    string methods

--- src/main/java/base/core/basic/classobject/CommonMethods.java
+++ src/main/java/base/core/basic/classobject/CommonMethods.java
@@ -52,37 +52,6 @@ public class CommonMethods {
         list.sort(null);
     }
 
-    public void stringMethods() {
-        String str = "Hello World";
-        String str2 = new String("Hello World");
-
-        int indexOfSubStr = str.indexOf("Hello");
-        
-        // String的长度length()方法
-        for (int i = 0; i < str.length() - 1; i++) {
-            System.out.println(str.charAt(i));
-        }
-        
-        // String到char[]的转换
-        char[] charArray = str2.toCharArray();
-
-        // char[]到String的转换
-        // String的构造函数可以接受char[]作为参数
-        String str3 = new String(charArray);
-        // String的静态方法valueOf()可以接受char[]作为参数
-        String str4 = String.valueOf(charArray);
-
-        // String[]到String的转换
-        String[] strArray = {"a", "b", "c", "d", "e"};
-        // String的静态方法join()可以接受String[]作为参数
-        String str5 = String.join("", strArray);
-        
-        for (char c : charArray) {
-            System.out.println(c);
-        }
-
-    }
-
     public void collectionStreamMethods() {
         List<Integer> list = new ArrayList<>();
         Set<Integer> set = new HashSet<>();
--- src/main/java/base/core/basic/classobject/MyString.java
+++ src/main/java/base/core/basic/classobject/MyString.java
@@ -34,4 +34,38 @@ public class MyString {
     }
 
     // The common methods of the String class include length(), charAt(), substring(), equals(), indexOf(), toLowerCase(), toUpperCase(), trim(), etc.
+    public void stringMethods() {
+        String str = "Hello World";
+        String str2 = new String("Hello World");
+
+        int indexOfSubStr = str.indexOf("Hello"); // Returns the index within this string of the first occurrence of the specified substring.
+        System.out.println(indexOfSubStr); // Output: 0
+        
+        String subStr = str.substring(0, 5); // Returns a new string that is a substring of this string, starting at the specified index and ending at the specified index.
+        System.out.println(subStr); // Output: Hello
+        
+        // String的长度length()方法
+        for (int i = 0; i < str.length() - 1; i++) {
+            System.out.println(str.charAt(i));
+        }
+        
+        // String到char[]的转换
+        char[] charArray = str2.toCharArray();
+
+        // char[]到String的转换
+        // String的构造函数可以接受char[]作为参数
+        String str3 = new String(charArray);
+        // String的静态方法valueOf()可以接受char[]作为参数
+        String str4 = String.valueOf(charArray);
+
+        // String[]到String的转换
+        String[] strArray = {"a", "b", "c", "d", "e"};
+        // String的静态方法join()可以接受String[]作为参数
+        String str5 = String.join("", strArray);
+        
+        for (char c : charArray) {
+            System.out.println(c);
+        }
+
+    }
 }
