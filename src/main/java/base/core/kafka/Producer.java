5f63e848adce3318833ed698088e93a2ca0436a8
Panda77 <PiperZzz@users.noreply.github.com>
2024-01-17 07:16:08 -0500

    kafka producer refine

--- src/main/java/base/core/kafka/Producer.java
+++ src/main/java/base/core/kafka/Producer.java
@@ -1,5 +1,15 @@
 package base.core.kafka;
 
+import org.springframework.kafka.core.KafkaTemplate;
+
 public class Producer {
-    
+    private final KafkaTemplate<String, Object> kafkaTemplateObjectMsg;
+
+    public Producer(KafkaTemplate<String, Object> kafkaTemplateObjectMsg) {
+        this.kafkaTemplateObjectMsg = kafkaTemplateObjectMsg;
+    }
+
+    public void eventStoreInit(String topicName, String content) {
+        kafkaTemplateObjectMsg.send(topicName, content);
+    }
 }
