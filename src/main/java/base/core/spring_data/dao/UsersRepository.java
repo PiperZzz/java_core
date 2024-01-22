e955ced8b5f29e95e6b5655d571dec2ceb82f5ee
Panda77 <PiperZzz@users.noreply.github.com>
2024-01-22 08:31:13 -0500

    update repository methods

--- src/main/java/base/core/spring_data/dao/UsersRepository.java
+++ src/main/java/base/core/spring_data/dao/UsersRepository.java
@@ -3,7 +3,12 @@ package base.core.spring_data.dao;
 import base.core.spring_data.model.Users;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import java.time.LocalDateTime;
+
 
 public interface UsersRepository extends JpaRepository<Users, Long> {
-    
+    Users findByEmail(String email);
+    Users findByUsername(String username);
+    List<Users> findByUpdatedAt(LocalDateTime updatedAt);
 }
