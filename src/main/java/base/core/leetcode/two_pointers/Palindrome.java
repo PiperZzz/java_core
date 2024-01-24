7298409230ef5d87c7f9d9700e80106247c6e613
Panda77 <PiperZzz@users.noreply.github.com>
2024-01-24 08:33:44 -0500

    comments

--- src/main/java/base/core/leetcode/two_pointers/Palindrome.java
+++ src/main/java/base/core/leetcode/two_pointers/Palindrome.java
@@ -8,17 +8,17 @@ public class Palindrome {
         str = str.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
         
         // Initialization - pointers
-        int left = 0;
-        int right = str.length() - 1;
+        int left = 0; // left pointer starts from the head of the string
+        int right = str.length() - 1; // right pointer starts from the tail of the string
         
-        while (left < right) { // When the left pointer is smaller than the right pointer or the two pointers meet, stop the loop
+        while (left < right) { // When the left pointer is smaller than the right pointer (not meet), execute the loop body
             if (str.charAt(left) != str.charAt(right)) {
-                return false;
+                return false; // When the current characters of the left and right pointers are not equal, return false
             }
-            left++;
-            right--;
+            left++; // Move the left pointer
+            right--; // Move the right pointer
         }
         
-        return true;
+        return true; // When the left pointer is equal to the right pointer (meet), return true
     }
 }
--- src/main/java/base/core/leetcode/two_pointers/ReverseString.java
+++ src/main/java/base/core/leetcode/two_pointers/ReverseString.java
@@ -2,26 +2,24 @@ package base.core.leetcode.two_pointers;
 
 public class ReverseString {
     public String reverseString(String str) {
-        // Boundary Condition Check
+        // Boundary/Corner Condition Check - Revisit
 
+        // Initialization - Convert the string to a character array
         char[] chars = str.toCharArray();
-        // 双指针初始化，left从头部开始遍历，right从尾部开始遍历
+        // Initialization - Pointers, left starts from the head of the string, right starts from the tail of the string
         int left = 0;
         int right = str.length() - 1;
         
-        // 当左右指针没有相遇时，执行循环体
-        while (left < right) {
-            // 交换左右指针指向的字符
+        while (left < right) { // When the left pointer is smaller than the right pointer (not meet), execute the loop body
+            // Swap the characters of the left and right pointers
             char temp = chars[left];
             chars[left] = chars[right];
             chars[right] = temp;
             
-            // 移动指针
-            left++;
-            right--;
+            left++; // Move the left pointer
+            right--; // Move the right pointer
         }
-        
-        // 将字符数组转换回字符串
-        return new String(chars);
+
+        return new String(chars); // Convert the character array back to a string
     }
 }
