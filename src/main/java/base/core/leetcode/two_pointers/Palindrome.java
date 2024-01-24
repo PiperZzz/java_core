ac1c68732825bf0bf49e5ef6eb5f49921b623469
Panda77 <fight4uking@gmail.com>
2024-01-24 07:18:25 -0500

    comments

--- src/main/java/base/core/leetcode/two_pointers/Palindrome.java
+++ src/main/java/base/core/leetcode/two_pointers/Palindrome.java
@@ -2,16 +2,16 @@ package base.core.leetcode.two_pointers;
 
 public class Palindrome {
     public boolean isPalindrome(String str) {
-        // Boundary Condition Check
+        // Boundary Condition Check - Revisit
 
-        // 删除非字母数字字符并将字符串转换为小写
+        // Remove all non-alphanumeric characters and convert the string to lowercase or Make reasonable assumptions
         str = str.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
         
-        // 使用双指针检查回文性质
+        // Initialization - pointers
         int left = 0;
         int right = str.length() - 1;
         
-        while (left < right) {
+        while (left < right) { // When the left pointer is smaller than the right pointer or the two pointers meet, stop the loop
             if (str.charAt(left) != str.charAt(right)) {
                 return false;
             }
@@ -20,5 +20,5 @@ public class Palindrome {
         }
         
         return true;
-    }    
+    }
 }
