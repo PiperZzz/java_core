719df90c6cedc8e5a528a665a2ab93735f876fff
Panda77 <PiperZzz@users.noreply.github.com>
2024-01-28 12:31:12 -0500

    comments

--- src/main/java/base/core/leetcode/sliding_window/MaxProfit.java
+++ src/main/java/base/core/leetcode/sliding_window/MaxProfit.java
@@ -2,26 +2,21 @@ package base.core.leetcode.sliding_window;
 
 public class MaxProfit {
     public static int maxProfit(int[] prices) {
-        // Boundary/Corner Case
+        // Boundary / Corner Case
         if (prices == null || prices.length <= 1) {
             return 0;
         }
 
-        // 初始化最低股价为正无穷大
-        int minPrice = Integer.MAX_VALUE;
-        // 初始化最大利润为0
+        // Init - minPrice set to the max value
+        int minPrice = Integer.MAX_VALUE; // 
+        // init - maxProfit set to 0
         int maxProfit = 0;
-        
-        /* 这种遍历方式维持了每一个价格出现的时间顺序，这点非常重要
-        因为只有这样才能保证最低股价在最大利润之前出现 */
-        for (int price : prices) {
-            // 更新最低股价
-            minPrice = Math.min(minPrice, price);
-            // 更新最大利润 
-            maxProfit = Math.max(maxProfit, price - minPrice);
-            // 更新最低股价并不会立即影响最大利润
+
+        for (int price : prices) { // 这种遍历方式维持了每一个价格出现的时间顺序，这点非常重要。因为只有这样才能保证最低股价在最大利润之前出现
+            minPrice = Math.min(minPrice, price); // Record the all time low price
+            maxProfit = Math.max(maxProfit, price - minPrice); // Max Profit 和 Current Price - Min Price 需要分开记录，更新最低股价并不会立即影响最大利润
         }
-            
+
         return maxProfit;
     }
 }
