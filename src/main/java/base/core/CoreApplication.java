284ca17604af813ccdbf7e06d253bbba2ed8835d
Panda77 <PiperZzz@users.noreply.github.com>
2024-02-02 13:19:23 -0500

    comment

--- src/main/java/base/core/CoreApplication.java
+++ src/main/java/base/core/CoreApplication.java
@@ -2,11 +2,16 @@ package base.core;
 
 import java.util.*;
 
+import java.util.concurrent.ConcurrentHashMap;
+
 import base.core.leetcode.ListNode;
 
 public class CoreApplication {
 	public static void main(String[] args) {
 		System.out.println(System.getProperty("java.version"));
+
+        ConcurrentHashMap<String, String> concurrentHashMap = new ConcurrentHashMap<>();
+        concurrentHashMap.put("1", "1");
 	}
 
     public static void printListNode() {
