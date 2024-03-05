8cafd460d36fd1eff80aa8894af13a9ddf32cc28
Panda77 <PiperZzz@users.noreply.github.com>
2024-03-05 14:23:55 -0500

    optional param

--- src/main/java/base/core/OrderController.java
+++ src/main/java/base/core/OrderController.java
@@ -1,6 +1,17 @@
 package base.core;
 
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
 import org.springframework.http.ResponseEntity;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,7 +30,7 @@ public class OrderController {
     }
 
     @GetMapping("/get")
-    public ResponseEntity<OrderDTO> getOrder(@RequestParam("orderId") Long orderId) {
+    public ResponseEntity<OrderDTO> getOrder(@RequestParam("orderId") Long orderId, @RequestParam(value = "userId", required = false) Long userId) {
         OrderDTO newOrder = orderService.getOrder(orderId);
         return ResponseEntity.ok(newOrder);
     }
