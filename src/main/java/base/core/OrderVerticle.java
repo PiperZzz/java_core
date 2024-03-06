e4f66d3a1b4e8fa48d92d575344f5307916e1e4a
Panda77 <PiperZzz@users.noreply.github.com>
2024-03-06 11:04:26 -0500

    add new vert.x route

--- src/main/java/base/core/OrderVerticle.java
+++ src/main/java/base/core/OrderVerticle.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Vertx;
 import io.vertx.core.http.HttpServer;
+import io.vertx.core.http.HttpServerRequest;
 
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
@@ -21,9 +22,45 @@ public class OrderVerticle extends AbstractVerticle{
         Router router = Router.router(vertx);
 
         router.route("/api/v1/order/create").handler(this::createOrder);
+        router.get("/api/v1/orders/get").handler(this::getOrderHandler);
+        router.put("/api/v1/orders/update/:orderId").handler(this::updateOrderHandler);
+        router.delete("/api/v1/orders/cancel/:orderId").handler(this::cancelOrderHandler);
+
+        server.requestHandler(router).listen(8080);
     }
 
     private void createOrder(RoutingContext routingContext) {
-        
+        HttpServerRequest request = routingContext.request();
+        request.bodyHandler(buffer -> {
+            OrderDTO order = new OrderDTO();
+            order.setOrderId(buffer.toJsonObject().mapTo(OrderDTO.class).getOrderId());
+            orderService.createOrder(order);
+            routingContext.response().end("Order created");
+        });
+    }
+
+    private void getOrderHandler(RoutingContext routingContext) {
+        HttpServerRequest request = routingContext.request();
+        Long orderId = Long.parseLong(request.getParam("orderId"));
+        OrderDTO order = orderService.getOrder(orderId);
+        routingContext.response().end(order.toString());
+    }
+
+    private void updateOrderHandler(RoutingContext routingContext) {
+        HttpServerRequest request = routingContext.request();
+        Long orderId = Long.parseLong(request.getParam("orderId"));
+        request.bodyHandler(buffer -> {
+            OrderDTO order = new OrderDTO();
+            order.setOrderId(buffer.toJsonObject().mapTo(OrderDTO.class).getOrderId());
+            OrderDTO newOrder = orderService.updateOrder(orderId, order);
+            routingContext.response().end("Order updated");
+        });
+    }
+
+    private void cancelOrderHandler(RoutingContext routingContext) {
+        HttpServerRequest request = routingContext.request();
+        Long orderId = Long.parseLong(request.getParam("orderId"));
+        orderService.cancelOrder(orderId);
+        routingContext.response().end("Order cancelled");
     }
 }
