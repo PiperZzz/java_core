96f6f22f11321a63eba3333b2b5a2181f0dd7355
Panda77 <PiperZzz@users.noreply.github.com>
2024-03-06 11:18:51 -0500

    update order entity

--- src/main/java/base/core/Order.java
+++ src/main/java/base/core/Order.java
@@ -6,12 +6,17 @@ import javax.persistence.Id;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 
+import lombok.Data;
+import lombok.NonNull;
+
 @Entity
 @Table(name = "Orders") // "Order" is a reserved keyword in SQL
+@Data
 public class Order {
-
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @NonNull
     private Long orderId;
 
     private String symbol;
@@ -19,4 +24,28 @@ public class Order {
     private double price;
     private int quantity;
     private String status; // "NEW", "FILLED", "PARTIALLY_FILLED", "CANCELLED"
+    
+    public Order(String symbol, String side, double price, int quantity, String status) {
+        this.symbol = symbol;
+        this.side = side;
+        this.price = price;
+        this.quantity = quantity;
+        this.status = status;
+    }
+
+    public Order() {
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "orderId=" + orderId +
+                ", symbol='" + symbol + '\'' +
+                ", side='" + side + '\'' +
+                ", price=" + price +
+                ", quantity=" + quantity +
+                ", status='" + status + '\'' +
+                '}';
+    }
+
 }
