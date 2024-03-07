2e0efed48133a176c62e43e70fdb827ec06f8913
Panda77 <PiperZzz@users.noreply.github.com>
2024-03-07 07:39:51 -0500

    TwoSum update map key value logic

--- src/main/java/base/core/leetcode/hashing/TwoSum.java
+++ src/main/java/base/core/leetcode/hashing/TwoSum.java
@@ -6,25 +6,25 @@ public class TwoSum {
     public static int[] twoSum(int[] nums, int target) {
         // Boundary Condition Check
         if (nums == null || nums.length <= 1) {
-            throw new IllegalArgumentException("Invalid input array");
+            return new int[] {}; // 如果数组为空或者长度小于等于1，则返回一个空数组
         }
 
-        // 用一个HashMap来存储数组中的元素，num是Key，num的index是Value
-        Map<Integer, Integer> map = new HashMap<>();
+        // 用一个HashMap来存储目标差值和数组中所对应的数，差值是Key，原数是Value
+        Map<Integer, Integer> mapComplementNum = new HashMap<>();
 
-        for (int i = 0; i < nums.length; i++) {
-            int complement = target - nums[i];
-
-            if (map.containsKey(complement)) {
-                // 如果差值complement存在于map的key中，则说明这个complement之前已经以某一个num的身份出现过
-                return new int[]{map.get(complement), i}; // 返回答案数组
-
+        for (int num : nums) {
+            if (mapComplementNum.containsKey(num)) {
+                // 如果num已经存在于map的key中，则说明这个num之前已经以complement的身份出现过
+                return new int[]{mapComplementNum.get(num), num}; // 那么这个Entry的Value和Key就是答案，循环结束
             }
 
-            // 如果差值complement不存在于map的key中，则将当前num添加到map中
-            map.put(nums[i], i);
+            // 计算当前数num和目标数target的差值
+            int complement = target - num; 
+
+            // 如果差值complement不存在于map的key中，则添加到map的Key，当前数num添加为对应的Value
+            mapComplementNum.put(complement, num); // 这步的作用是遍历过的差值会立即以Key的身份出现在循环的第一步检查中 - Line 16
         }
         
-        throw new IllegalArgumentException("No two sum solution");
+        return new int[] {}; // 如果没有找到答案，则返回一个空数组
     }
 }
